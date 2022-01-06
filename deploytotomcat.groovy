fb6842e7f2f2df8961f4604a28cecc04e09209dd
Chandu2407 <76043036+Chandu2407@users.noreply.github.com>
2022-01-06 22:24:12 +0530

    Delete deploytotomcat.groovy

--- deploytotomcat.groovy
+++
@@ -1,9 +0,0 @@
-def call(ip,instanceid){
-   sshagent([instanceid]) {
-     sh "scp -o StrictHostKeyChecking=no target/*.war ubuntu@${ip}:/opt/tomcat/apache-tomcat-9.0.56/webapps/javademoproject.war"
-                    sh "ssh ubuntu@${ip} /opt/tomcat/apache-tomcat-9.0.56/bin/shutdown.sh"
-                    sh "ssh ubuntu@${ip} /opt/tomcat/apache-tomcat-9.0.56/bin/startup.sh"
-                }
-  echo "Deploy to tomcat on ip ${ip,instanceid}"
-                
-            }
